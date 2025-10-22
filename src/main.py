7fd8d7880c3550ab14e57692ac1c884a6e2f81b2
Valeria Santos <valeria.redesociais@gmail.com>
2025-10-22 20:39:48 -0300

    Delete main.py

--- src/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from src.app.api.controller.stocks import router as stocks_router
-
-app = FastAPI()
-
-app.include_router(stocks_router, prefix="/api/controller/stocks")
